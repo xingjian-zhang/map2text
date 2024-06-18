0f1ef2a3e9d704c07b6308da1d90046368d8cd29
Xingjian Zhang <jimmyzxj@umich.edu>
2024-06-18 11:49:52 -0400

    Add chat API.

--- llm4explore/model/common.py
+++ llm4explore/model/common.py
@@ -2,7 +2,7 @@
 import asyncio
 import json
 import os
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 
@@ -17,7 +17,7 @@ def process_embedding_requests(
     if model_name == "text-embedding-ada-002":
         # Write data into a temporary jsonl files
         os.makedirs("tmp", exist_ok=True)
-        requests_filepath = "tmp/requests.jsonl"
+        requests_filepath = "tmp/embedding-requests.jsonl"
         save_filepath = "tmp/text-embedding-ada-002-embeddings.jsonl"
         with open(requests_filepath, "w") as f:
             for i, text in enumerate(data):
@@ -45,9 +45,53 @@ def process_embedding_requests(
                 embedding = response[1]["data"][0]["embedding"]
                 idx = response[-1]["id"]
                 id2embedding[idx] = embedding
-        assert len(data) == len(id2embedding)
+        assert len(data) == len(id2embedding), f"{len(data)} != {len(id2embedding)}"
         embeddings = np.array([id2embedding[i] for i in range(len(data))])
     else:
         raise ValueError(f"Model {model_name} not supported.")
 
     return embeddings
+
+
+def process_chat_requests(
+    model_name: str,
+    messages: List[List[Dict[str, str]]],
+    **kwargs,
+) -> List[str]:
+    if model_name in {"gpt-35-turbo", "gpt-4"}:
+        # Write data into a temporary jsonl files
+        os.makedirs("tmp", exist_ok=True)
+        requests_filepath = "tmp/chat-requests.jsonl"
+        save_filepath = f"tmp/{model_name}-chats.jsonl"
+        with open(requests_filepath, "w") as f:
+            for i, message in enumerate(messages):
+                f.write(
+                    json.dumps({
+                        "model": model_name,
+                        "messages": message,
+                        "metadata": {
+                            "id": i
+                        },
+                    }) + "\n")
+        # Send requests to the API
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            api.process_api_requests_from_file(
+                requests_filepath=requests_filepath,
+                save_filepath=save_filepath,
+                **kwargs,
+            ))
+        # Read the chat responses and note the order
+        id2chat = {}
+        with open(save_filepath, "r") as f:
+            for line in f:
+                response = json.loads(line)
+                chat = response[1]["choices"][0]["message"]["content"]
+                idx = response[-1]["id"]
+                id2chat[idx] = chat
+        assert len(messages) == len(id2chat), f"{len(messages)} != {len(id2chat)}"
+        chats = [id2chat[i] for i in range(len(messages))]
+    else:
+        raise ValueError(f"Model {model_name} not supported.")
+
+    return chats
--- test/test_process_api_requests.py
+++ test/test_process_api_requests.py
@@ -4,7 +4,8 @@ import tempfile
 
 import pytest
 
-from llm4explore.model.common import process_embedding_requests
+from llm4explore.model.common import (process_chat_requests,
+                                      process_embedding_requests)
 
 
 @pytest.fixture
@@ -33,3 +34,35 @@ def test_process_embedding_requests(change_dir):
         logging_level=logging.INFO,
     )
     assert embeddings.shape == (3, 1536)
+
+
+def test_process_chat_requests(change_dir):
+    test_data = [
+        [{
+            "role": "system",
+            "content": "You are a helpful assistant."
+        }, {
+            "role": "user",
+            "content": "What is the weather like today?"
+        }],
+        [{
+            "role": "system",
+            "content": "You are a smart assistant."
+        }, {
+            "role": "user",
+            "content": "What is the meaning of life?"
+        }],
+    ]
+    responses = process_chat_requests(
+        "gpt-35-turbo",
+        test_data,
+        request_url="https://embedding-api.openai.azure.com/openai/deployments/"
+        "gpt-35-turbo/chat/completions?api-version=2023-12-01-preview",
+        api_key=os.getenv("AZURE_OPENAI_KEY"),
+        max_requests_per_minute=100,
+        max_tokens_per_minute=1000,
+        token_encoding_name="cl100k_base",
+        max_attempts=5,
+        logging_level=logging.INFO,
+    )
+    assert len(responses) == 2
