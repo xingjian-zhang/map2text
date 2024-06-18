9e2335e610ab27561c5d22ec940fe25a21ec9fb4
Xingjian Zhang <jimmyzxj@umich.edu>
2024-06-18 14:04:22 -0400

    Add KNNSampler class for KNN search.

--- llm4explore/model/common.py
+++ llm4explore/model/common.py
@@ -2,8 +2,9 @@
 import asyncio
 import json
 import os
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
 
+from annoy import AnnoyIndex
 import numpy as np
 import hashlib
 
@@ -46,7 +47,8 @@ def process_embedding_requests(
                 embedding = response[1]["data"][0]["embedding"]
                 idx = response[-1]["id"]
                 id2embedding[idx] = embedding
-        assert len(data) == len(id2embedding), f"{len(data)} != {len(id2embedding)}"
+        assert len(data) == len(
+            id2embedding), f"{len(data)} != {len(id2embedding)}"
         embeddings = np.array([id2embedding[i] for i in range(len(data))])
     else:
         raise ValueError(f"Model {model_name} not supported.")
@@ -90,7 +92,8 @@ def process_chat_requests(
                 chat = response[1]["choices"][0]["message"]["content"]
                 idx = response[-1]["id"]
                 id2chat[idx] = chat
-        assert len(messages) == len(id2chat), f"{len(messages)} != {len(id2chat)}"
+        assert len(messages) == len(
+            id2chat), f"{len(messages)} != {len(id2chat)}"
         chats = [id2chat[i] for i in range(len(messages))]
     else:
         raise ValueError(f"Model {model_name} not supported.")
@@ -101,3 +104,80 @@ def process_chat_requests(
 def hash_array(array: np.ndarray) -> str:
     """Hash the array using MD5 and return the hash as a string."""
     return hashlib.md5(array.tobytes()).hexdigest()
+
+
+def check_tmp_dir(func):
+    """Function decorator to check the existence of tmp directory."""
+
+    def wrapper(*args, **kwargs):
+        os.makedirs("tmp", exist_ok=True)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@check_tmp_dir
+class KNNSampler():
+    """Sampler for KNN search."""
+
+    def __init__(self,
+                 knn_embeddings: np.ndarray,
+                 metric: str = "euclidean",
+                 n_trees: int = 10,
+                 k: int = 5,
+                 check_leakage: bool = True):
+        """Initialize the sampler.
+
+        Args:
+            knn_embeddings (np.ndarray): Embeddings to sample from. Usually the
+                low-dimensional embeddings.
+            metric (str): Distance metric.
+            n_trees (int): Number of trees.
+            k (int): Number of neighbors to search for.
+            check_leakage (bool): Whether to check for leakage.
+        """
+        assert isinstance(knn_embeddings, np.ndarray)
+        dim = knn_embeddings.shape[1]
+        hash_str = hash_array(knn_embeddings)
+
+        self.cache_path = f"tmp/knn_cache_{hash_str}_{metric}_{n_trees}_{k}.ann"
+        self.index = AnnoyIndex(dim, metric)
+        self.knn_embeddings = knn_embeddings
+        self.metric = metric
+        self.n_trees = n_trees
+        self.k = k
+        self.check_leakage = check_leakage
+
+        self._init_knn()
+
+    def _init_knn(self):
+        if os.path.exists(self.cache_path):
+            print("Loading KNN index from cache.")
+            self.index.load(self.cache_path)
+        else:
+            print("Building KNN index.")
+            for i, embedding in enumerate(self.knn_embeddings):
+                self.index.add_item(i, embedding)
+            self.index.build(self.n_trees)
+            self.index.save(self.cache_path)
+        print("KNN index initialized.")
+
+    def sample(self, query: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """Get k nearest neighbors of the query.
+
+        Args:
+            query (np.ndarray): The query vector.
+
+        Returns:
+            The indices and distances of the k nearest neighbors.
+        """
+        indices, dists = self.index.get_nns_by_vector(
+            query,
+            self.k,
+            include_distances=True,
+        )
+        if self.check_leakage and any(dists[i] < 1e-6
+                                      for i in range(len(dists))):
+            raise ValueError("Query is too close to a sample."
+                             "The samples may contain query itself.")
+        return indices, dists
---
+++ test/test_knn_sampler.py
@@ -0,0 +1,60 @@
+import pytest
+from llm4explore.model.common import KNNSampler
+import numpy as np
+
+
+def test_retrieve_correct_knns():
+    # Generate 10 hand-crafted vectors
+    knn_embeddings = np.array([
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7],
+        [4, 5, 6, 7, 8],
+        [5, 6, 7, 8, 9],
+        [6, 7, 8, 9, 10],
+        [7, 8, 9, 10, 11],
+        [8, 9, 10, 11, 12],
+        [9, 10, 11, 12, 13],
+        [10, 11, 12, 13, 14],
+    ])
+    # Create KNNSampler instance
+    sampler = KNNSampler(knn_embeddings=knn_embeddings, k=2)
+
+    # Define a query vector
+    query = np.array([3.5, 4.5, 5.5, 6.5, 7.5])
+
+    # Get the k nearest neighbors
+    index, dist = sampler.sample(query)
+
+    # Check if the correct indices are retrieved
+    assert len(index) == 2
+    assert 2 in index
+    assert 3 in index
+
+    # Check if the distances are correct
+    assert np.allclose(dist, np.sqrt(0.5**2 * 5))
+
+
+def test_check_leakage_knns():
+    # Generate 10 hand-crafted vectors
+    knn_embeddings = np.array([
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7],
+        [4, 5, 6, 7, 8],
+        [5, 6, 7, 8, 9],
+        [6, 7, 8, 9, 10],
+        [7, 8, 9, 10, 11],
+        [8, 9, 10, 11, 12],
+        [9, 10, 11, 12, 13],
+        [10, 11, 12, 13, 14],
+    ])
+    # Create KNNSampler instance
+    sampler = KNNSampler(knn_embeddings=knn_embeddings, k=2, check_leakage=True)
+
+    # Define a query vector (present in the knn_embeddings)
+    query = np.array([3, 4, 5, 6, 7])
+
+    with pytest.raises(ValueError):
+        # Get the k nearest neighbors
+        index, dist = sampler.sample(query)
