44b0e51f38deda1746b1f180960c0dfbefcda4f6
Xingjian Zhang <jimmyzxj@umich.edu>
2024-06-23 13:00:12 -0400

    Update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
+
+def load_requirements(filename="requirements.txt"):
+    with open(filename, "r") as f:
+        lineiter = (line.strip() for line in f)
+        return [line for line in lineiter if line and not line.startswith("#")]
+
+
 setup(
     name="llm4explore",
     version="0.1",
     packages=find_packages(),
+    install_requires=load_requirements(),
 )
