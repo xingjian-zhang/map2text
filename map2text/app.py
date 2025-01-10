72926fe1bb4c787cf1b0cbefbacbb5131519b1c8
sx-liu <142147915+sx-liu@users.noreply.github.com>
2025-01-10 14:15:41 -0500

    Update app with generation (#20)
    Co-authored-by: Sx Lau <3211220990@qq,com>

--- map2text/app.py
+++ map2text/app.py
@@ -2,10 +2,12 @@ import textwrap
 from pathlib import Path
 
 import numpy as np
+import os
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
+import yaml
+
 
 GRID_COLOR = "rgba(128,128,128,0.1)"
 POINT_COLOR = "rgba(128,128,128,0.9)"
@@ -16,6 +18,7 @@ def rewrap_br(text, **kwargs):
     return text.replace("\n", "<br>")
 
 
+@st.cache_data
 def load_data(dataset_name):
     project_root = Path(__file__).parent.parent
     data_files = {
@@ -49,11 +52,11 @@ def load_data(dataset_name):
     df.rename(columns={data_file["target_col"]: "display_text"}, inplace=True)
     df["display_text"] = df["display_text"].apply(rewrap_br, width=80)
     embeddings = np.load(embedding_path)
-    return df, embeddings["low_dim_embeddings"]
+    return df, embeddings["low_dim_embeddings"], embeddings["high_dim_embeddings"]
 
 
-def generate_plotly_figure(dataset_name):
-    df, embeddings = load_data(dataset_name)
+@st.cache_data
+def generate_plotly_figure(df, embeddings):
     df["x"] = embeddings[:, 0]
     df["y"] = embeddings[:, 1]
     fig = go.Figure()
@@ -99,20 +102,131 @@ def generate_plotly_figure(dataset_name):
     return fig
 
 
+def load_generator(generator_type, dataset_name, data, low_dim_embeddings, high_dim_embeddings):
+    from model import non_trainable_gen, trainable_ffn
+    project_root = Path(__file__).parent
+    generation_configs = {
+        "plagiarism": "plagiarism.yaml",
+        "embedding": "vec2text_uw.yaml",
+        "embedding_ffn": "vec2text_ffn_inference.yaml",
+        "prompting": "gpt4o_fs.yaml",
+    }
+    config_file = (project_root / "configs" / dataset_name.replace("-", "_") /
+                   generation_configs[generator_type])
+    with open(config_file, "r") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+    # Load complementary data
+    times = data[config["data"]["time_col"]]
+    time_split = config["data"]["time_split"]
+    targets = data["display_text"]
+    targets_old = targets[times < time_split].tolist()
+    low_dim_embeddings_old = low_dim_embeddings[times < time_split]
+    n_dims = low_dim_embeddings.shape[1]
+
+    # Initialize the generator.
+    generator_type = config["method"]["type"]
+    if generator_type == "plagiarism":
+        generator = non_trainable_gen.PlagiarismGenerator(
+            n_dims=n_dims,
+            data_old=targets_old,
+            low_dim_embeddings_old=low_dim_embeddings_old,
+            **config["method"]["init_args"],
+        )
+    elif generator_type == "embedding":
+        high_dim_embeddings_old = high_dim_embeddings[times < time_split]
+        generator = non_trainable_gen.EmbeddingInversionGenerator(
+            n_dims=n_dims,
+            data_old=targets_old,
+            low_dim_embeddings_old=low_dim_embeddings_old,
+            high_dim_embeddings_old=high_dim_embeddings_old,
+            **config["method"]["init_args"],
+        )
+    elif generator_type == "embedding_ffn":
+        generator = trainable_ffn.EmbeddingInversionFFNGenerator(
+            n_dims=n_dims,
+            data_old=targets_old,
+            low_dim_embeddings_old=low_dim_embeddings_old,
+            **config["method"]["init_args"],
+        )
+    elif generator_type == "prompting":
+        generator = non_trainable_gen.RetrievalAugmentedGenerator(
+            target= config["data"]["target_col"],
+            n_dims=n_dims,
+            texts=targets_old,
+            low_dim_embeddings=low_dim_embeddings_old,
+            times=times[times < time_split].values,
+            **config["method"]["init_args"],
+        )
+    else:
+        raise ValueError(f"Unknown generator type: {generator_type}.")
+
+    return generator
+
+
 # Wide mode, dark mode
 st.set_page_config(layout="wide")
 st.title("Map2Text Demo")
 
-# Select dataset
-
+# Initialize marked coordinates
+marked_coords = None
+generated_text = None
+os.environ["OPENAI_API_KEY"] = ""
 
 col1, col2 = st.columns([1, 1])
-with col2:
+
+with col1:
+    # Select dataset
     dataset_name = st.selectbox(
         "Select dataset",
         ["red_team_attempts", "persona", "cs-research-idea", "cs-research-context"],
     )
-    st.write("Hello")
-with col1:
-    fig = generate_plotly_figure(dataset_name)
-    st.plotly_chart(fig, theme=None, use_container_width=False)
+
+    generator_type = st.selectbox(
+        "Select generator",
+        ["plagiarism", "embedding", "embedding_ffn", "prompting"],
+    )
+
+with col2:
+    # Text boxes for coordinates
+    x_coord = st.text_input("Enter X coordinate:")
+    y_coord = st.text_input("Enter Y coordinate:")
+
+# Set api key if applicable
+os.environ["OPENAI_API_KEY"] = st.text_input("(Optional) OpenAI API Key:")
+
+# Button to mark coordinate
+if st.button("Generate"):
+    if x_coord and y_coord:
+        try:
+            x = float(x_coord)
+            y = float(y_coord)
+            # st.write(f"Marking coordinate: ({x}, {y})")
+            marked_coords = (x, y)
+        except ValueError:
+            st.error("Please enter valid numeric values for the coordinates.")
+    else:
+        st.error("Both X and Y coordinates must be provided.")
+
+# Create the Streamlit Plotly chart with optional marked point
+df, low_dim_embeddings, high_dim_embeddings = load_data(dataset_name)
+if marked_coords is not None:
+    generator = load_generator(
+        generator_type, dataset_name, df, low_dim_embeddings, high_dim_embeddings,
+    )
+    generated_text = generator.decode(marked_coords)[0]
+
+# Generate plot
+fig = generate_plotly_figure(df, low_dim_embeddings)
+if marked_coords:
+    fig.add_trace(
+        go.Scatter(
+            x=[marked_coords[0]],
+            y=[marked_coords[1]],
+            mode="markers",
+            marker=dict(size=10, color="red"),
+            hovertemplate="%{text}<extra></extra>",
+            text=[generated_text],
+        )
+    )
+st.plotly_chart(fig, theme=None, use_container_width=True)
